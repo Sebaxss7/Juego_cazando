79a99e09130f5a3f7f46b6d81e822116bee8fbef
Sebaxss7 <totasigjordy60@gmail.com>
2026-09-15 07:22:18 -0500

    corregido hasta el numeral 18

--- cazando.js
+++ cazando.js
@@ -1,7 +1,27 @@
 const canvas = document.getElementById("areaJuego");
 const contexto = canvas.getContext("2d");
 
+let gatoX = 200;
+let gatoY = 200;
+let comidaX = 450;
+let comidaY = 450;
+
+const altoGato = 100;
+const anchoGato = 100;
+const altoComida = 50;
+const anchoComida = 50;
+
 function graficarGato() {
     contexto.fillStyle = "blue";
-    contexto.fillRect(200, 200, 100, 100);
+    contexto.fillRect(gatoX, gatoY, anchoGato, altoGato);
+}
+
+function graficarComida() {
+    contexto.fillStyle = "red";
+    contexto.fillRect(comidaX, comidaY, anchoComida, altoComida);
+}
+
+function iniciarJuego() {
+    graficarGato();
+    graficarComida();
 }
