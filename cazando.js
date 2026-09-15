073ad025a39cd4a14c8db273cfaec28bca32ca58
Sebaxss7 <totasigjordy60@gmail.com>
2026-09-15 07:30:18 -0500

    corregido y finalizado

--- cazando.js
+++ cazando.js
@@ -1,27 +1,54 @@
 const canvas = document.getElementById("areaJuego");
 const contexto = canvas.getContext("2d");
 
-let gatoX = 200;
-let gatoY = 200;
-let comidaX = 450;
-let comidaY = 450;
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
 
 const altoGato = 100;
 const anchoGato = 100;
 const altoComida = 50;
 const anchoComida = 50;
 
+function graficarRectangulo(x, y, ancho, alto, color) {
+    contexto.fillStyle = color;
+    contexto.fillRect(x, y, ancho, alto);
+}
+
 function graficarGato() {
-    contexto.fillStyle = "blue";
-    contexto.fillRect(gatoX, gatoY, anchoGato, altoGato);
+    graficarRectangulo(
+        gatoX,
+        gatoY,
+        anchoGato,
+        altoGato,
+        "gray"
+    );
 }
 
 function graficarComida() {
-    contexto.fillStyle = "red";
-    contexto.fillRect(comidaX, comidaY, anchoComida, altoComida);
+    graficarRectangulo(
+        comidaX,
+        comidaY,
+        anchoComida,
+        altoComida,
+        "red"
+    );
 }
 
 function iniciarJuego() {
+
+    // Gato centrado
+    gatoX = (canvas.width - anchoGato) / 2;
+    gatoY = (canvas.height - altoGato) / 2;
+
+    // Comida en la esquina inferior derecha
+    comidaX = canvas.width - anchoComida;
+    comidaY = canvas.height - altoComida;
+
     graficarGato();
     graficarComida();
 }
+
+
+
